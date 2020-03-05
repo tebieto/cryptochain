d51477063c652d85b91a201f7a212c01e07c6dcc
Terry Ebieto <tebieto@gmail.com>
2020-03-05 15:06:07 +0100

    first commit

---
+++ block.test.js
@@ -0,0 +1,44 @@
+const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
+
+describe('Block', ()=> {
+    const timestamp = 'a-date';
+    const lastHash = 'foo-hash';
+    const hash = 'bar-hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block({timestamp, lastHash, hash, data});
+
+    it('has a timestamp, lastHash, hash, and data property', () => {
+        expect(block.timestamp).toEqual(timestamp)
+        expect(block.lastHash).toEqual(lastHash)
+        expect(block.hash).toEqual(hash)
+        expect(block.data).toEqual(data)
+    });
+
+    describe('genesis()', () => {
+
+        const genesisBlock = Block.genesis();
+        it('returns a Block instance', ()=> {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns the genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
+
+    });
+
+    describe('mineBlock()', () => {
+        const lastBlock = Block.genesis();
+        const data = 'mined data';
+        const mineBlock = Block.mineBlock({ lastBlock, data });
+
+        it('returns a Block instance', () => {
+            expect(minedBlock instanceof Block).toBe(true)
+        });
+
+        it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+            expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+        });
+    });
+});
