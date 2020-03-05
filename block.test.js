8bffa87fe5fd713ab0545ccb11c488bac94bb757
Terry Ebieto <tebieto@gmail.com>
2020-03-05 16:56:43 +0100

    added a crypto hash function

--- block.test.js
+++ block.test.js
@@ -31,7 +31,7 @@ describe('Block', ()=> {
     describe('mineBlock()', () => {
         const lastBlock = Block.genesis();
         const data = 'mined data';
-        const mineBlock = Block.mineBlock({ lastBlock, data });
+        const minedBlock = Block.mineBlock({ lastBlock, data });
 
         it('returns a Block instance', () => {
             expect(minedBlock instanceof Block).toBe(true)
@@ -39,6 +39,14 @@ describe('Block', ()=> {
 
         it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
             expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+        });
+
+        it('sets a `data`', () => {
+            expect(minedBlock.data).toEqual(data)
+        });
+
+        it('sets a `timestamp`', () => {
+            expect(minedBlock.timestamp).not.toEqual(undefined)
         });
     });
 });
