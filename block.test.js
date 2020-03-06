8af132f719641b647f1c6d9cbefecf7e2b73b2fa
Terry Ebieto <tebieto@gmail.com>
2020-03-06 10:46:29 +0100

    completed mineBlock()

--- block.test.js
+++ block.test.js
@@ -1,5 +1,6 @@
 const Block = require('./block');
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash')
 
 describe('Block', ()=> {
     const timestamp = 'a-date';
@@ -47,6 +48,10 @@ describe('Block', ()=> {
 
         it('sets a `timestamp`', () => {
             expect(minedBlock.timestamp).not.toEqual(undefined)
+        });
+
+        it('Creates a SHA-256 `hash` based on the proper inputs', () => {
+            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data))
         });
     });
 });
