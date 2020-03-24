47200c6257711bdc77c2fade7361518eb9ccf2b3
Terry Ebieto <tebieto@gmail.com>
2020-03-24 14:54:54 +0100

    setup react app, fetched and rendered blocks and WalletInfo  onComponentDidMount

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS =   `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
@@ -79,7 +80,7 @@ app.get('/api/wallet-info', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 
 const syncWithRootState = () => {
@@ -98,6 +99,43 @@ const syncWithRootState = () => {
             transactionPool.setMap(rootTransactionPoolMap);
         }
     });
+}
+
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({wallet, recipient, amount}) => {
+    const transaction = wallet.createTransaction({
+        recipient, amount, chain:  blockchain.chain
+    });
+
+    transactionPool.setTransaction(transaction);
+}
+
+const walletAction = () => generateWalletTransaction({
+    wallet, recipient: walletFoo.publicKey, amount: 5
+});
+
+const walletFooAction = () => generateWalletTransaction({
+    walletFoo, recipient: walletBar.publicKey, amount: 10
+});
+
+const walletBarAction = () => generateWalletTransaction({
+    walletBar, recipient: wallet.publicKey, amount: 15
+});
+
+
+for(let i=0; i<10; i++) {
+    if(i%3 === 0) {
+        walletAction();
+        walletFooAction();
+    } else if(i%3 === 1) {
+        walletAction();
+        walletBarAction();
+    } else {
+        walletFooAction();
+        walletBarAction();
+    }
 }
 
 let PEER_PORT;
