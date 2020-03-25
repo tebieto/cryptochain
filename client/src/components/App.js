708674948d6530d162baffa93ff985a1e85ca622
Terry Ebieto <tebieto@gmail.com>
2020-03-25 18:41:48 +0100

    Installed and setting up react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
 import logo from '../assets/logo.png';
 
 class App extends Component {
@@ -26,7 +25,6 @@ class App extends Component {
                 <div>Wallet Balance: {balance}</div>    
                 </div>
                 <br />
-                <Blocks />
             </div>
         );
     }
